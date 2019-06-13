92644cdb291fe30257cbf3234ddfffe7188c68ab
Shuvendu Dhal <shuvenduoffline@gmail.com>
2019-06-13 21:54:34 +0530

    test

---
+++ app/src/main/java/com/example/attendancetaking/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.attendancetaking;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),AttendanceMain.class));
+                finish();
+            }
+        },1500);
+    }
+}
